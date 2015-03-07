8b5bd3f88b7b1d7f59245eec644b891678b80ffd
hqmq <michael@riesd.com>
2015-03-06 23:20:41 -0700

    setup code coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
+if ENV['REPORT_COVERAGE']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'protobuffness'
