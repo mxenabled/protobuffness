c6391ca39f5742e84d8c19bbaa5edaf37344f7dc
hqmq <michael@riesd.com>
2015-03-06 23:50:41 -0700

    better test

--- spec/integration/simple_encoding_spec.rb
+++ spec/integration/simple_encoding_spec.rb
@@ -1,11 +1,16 @@
-RSpec.describe "encoding a very simple message" do
-  let(:schema) {
-    ::Protobuffness::Schema.new([
-      ::Protobuffness::Field::String.new(:required, :description, 1),
-    ])
-  }
-  let(:values) { {:description => "crispy"} }
-  subject { ::Protobuffness.encode(values, schema) }
+RSpec.describe "encoding very simple messages" do
+  context "single required string" do
+    let(:schema) {
+      ::Protobuffness::Schema.new([
+        ::Protobuffness::Field::String.new(:required, :description, 1),
+      ])
+    }
 
-  specify { expect(subject.encode).to eq binary_string("\n\x06crispy") }
+    def encode(description)
+      ::Protobuffness.encode({:description => description}, schema)
+    end
+
+    specify { expect(encode("bob")).to eq binary_string("\n\x03bob") }
+    specify { expect(encode("crispy")).to eq binary_string("\n\x06crispy") }
+  end
 end
