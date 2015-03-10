f700b9178aa3d5400f3cec14696ada06a3b43d85
hqmq <michael@riesd.com>
2015-03-10 13:33:55 -0600

    support enum objects, tags, and labels as arguments

--- spec/enum_message_spec.rb
+++ spec/enum_message_spec.rb
@@ -9,20 +9,30 @@ RSpec.describe "enum message encoding" do
   #   optional Crunchiness chrunchiness = 2;
   # }
   class Crunchiness
-    VALUES = {
+    VALUES_BY_LABEL = {
       :FATTY  => 0,
       :CRISPY => 1,
       :CEMENT => 2,
     }.freeze
 
+    VALUES_BY_TAG = VALUES_BY_LABEL.invert.freeze
+
+    def self.lookup(value)
+      return value if value.is_a?(Crunchiness)
+      return new(value) if value.is_a?(Symbol)
+      label = VALUES_BY_TAG.fetch(value) { raise ArgumentError, "unknown Crunchiness value: #{value}" }
+      return new(label)
+    end
+
     attr_reader :label
 
     def initialize(label)
+      raise ArgumentError, "unkown Crunchiness value: #{label}" unless VALUES_BY_LABEL.keys.include?(label)
       @label = label
     end
 
     def tag
-      VALUES[label]
+      VALUES_BY_LABEL[label]
     end
   end
 
@@ -52,7 +62,7 @@ RSpec.describe "enum message encoding" do
     end
 
     def crunchiness=(crunchiness)
-      @attributes[:crunchiness] = Crunchiness.new(crunchiness)
+      @attributes[:crunchiness] = Crunchiness.lookup(crunchiness)
     end
 
     ## Encoding
@@ -85,5 +95,19 @@ RSpec.describe "enum message encoding" do
   it "encodes a cement bacon" do
     bacon = Bacon.new(:id => 25_432, :crunchiness => :CEMENT)
     expect(bacon.encode).to eq binary_string("\b\xD8\xC6\x01\x10\x02")
+  end
+
+  it "can specify the tag for an enum" do
+    bacon = Bacon.new(:id => 456, :crunchiness => 1)
+    expect(bacon.encode).to eq binary_string("\b\xC8\x03\x10\x01")
+  end
+
+  it "can specify an enum object" do
+    bacon = Bacon.new(:id => 456, :crunchiness => Crunchiness.new(:CRISPY))
+    expect(bacon.encode).to eq binary_string("\b\xC8\x03\x10\x01")
+  end
+
+  it "rejects fake values" do
+    expect{ Bacon.new(:crunchiness => :WAT) }.to raise_error(ArgumentError)
   end
 end
