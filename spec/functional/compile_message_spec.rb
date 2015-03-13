33e14812d230ef4565dfe96efc6764b02cb5744f
Michael Ries <michael@riesd.com>
2015-03-13 16:53:41 -0600

    compile basic getters and setters

--- spec/functional/compile_message_spec.rb
+++ spec/functional/compile_message_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe "compiling a simple message" do
   it "defines a Sally class" do
     expect(defined?(Sally)).to eq "constant"
   end
+
+  it "generates getters and setters" do
+    sally = Sally.new
+    sally.mood = "grumpy"
+    sally.age = 9
+    expect(sally.mood).to eq "grumpy"
+    expect(sally.age).to eq 9
+  end
 end
