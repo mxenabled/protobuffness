56605e15551baf52022a556c2a678539350d78d8
hqmq <michael@riesd.com>
2015-03-13 22:23:51 -0600

    generate code by lines
    Generating by lines makes it easier to handle nesting and indentation

--- lib/protobuffness/compiler/message.rb
+++ lib/protobuffness/compiler/message.rb
@@ -5,46 +5,48 @@ module Protobuffness
 
       def initialize(message_type)
         @message_type = message_type
-        compile
       end
 
       def message_definition
-        stream.string
+        compile
       end
 
       private
 
       def compile
-        stream.puts "class #{message_type.name}"
-        define_initializer
-        define_attribute_methods
-        stream.puts "end"
-      end
-
-      def define_attribute_methods
-        message_type.field.each do |field|
-          define_methods_for(field)
+        define_class do
+          define_initializer << define_attribute_methods
         end
       end
 
+      def define_attribute_methods
+        message_type.field.map do |field|
+          define_methods_for(field)
+        end.flatten
+      end
+
+      def define_class
+        ["class #{message_type.name}"] << yield << ["end"]
+      end
+
       def define_initializer
-        stream.puts "  def initialize"
-        stream.puts "    @attributes = {}"
-        stream.puts "  end"
+        [
+          "def initialize",
+          "  @attributes = {}",
+          "end"
+        ]
       end
 
       def define_methods_for(field)
-        stream.puts "  def #{field.name}"
-        stream.puts "    @attributes[:#{field.name}]"
-        stream.puts "  end"
-        stream.puts ""
-        stream.puts "  def #{field.name}=(#{field.name})"
-        stream.puts "    @attributes[:#{field.name}] = #{field.name}"
-        stream.puts "  end\n"
-      end
-
-      def stream
-        @stream ||= StringIO.new
+        [
+          "def #{field.name}",
+          "  @attributes[:#{field.name}]",
+          "end",
+          "",
+          "def #{field.name}=(#{field.name})",
+          "  @attributes[:#{field.name}] = #{field.name}",
+          "end",
+        ]
       end
     end
   end
